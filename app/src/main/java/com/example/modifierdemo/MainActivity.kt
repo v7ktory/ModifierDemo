d42a1aad72ba20d900de06a8e935e37ac52fb0e2
v7ktory <v7ktory@gmail.com>
2025-10-02 19:55:01 +0700

    added second modifier

--- app/src/main/java/com/example/modifierdemo/MainActivity.kt
+++ app/src/main/java/com/example/modifierdemo/MainActivity.kt
@@ -46,6 +46,9 @@ fun DemoScreen(modifier: Modifier = Modifier) {
     val mymodifier = modifier
         .border(width = 2.dp, color = Color.Black)
         .padding(all = 10.dp)
+
+    val secondModifier = Modifier.height(100.dp)
+
     Column(
         Modifier.padding(20.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -53,7 +56,7 @@ fun DemoScreen(modifier: Modifier = Modifier) {
     ) {
         Text(
             "Hello Compose",
-            mymodifier,
+            mymodifier.then(secondModifier),
             fontSize = 40.sp,
             fontWeight = FontWeight.Bold
         )
